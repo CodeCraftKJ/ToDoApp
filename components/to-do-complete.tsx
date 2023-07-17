c987893702d7b7c9dbfd861466c2dcd57a7a9a7e
CodeCraftKJ <krzysztofjofficial@gmail.com>
2023-07-17 21:46:51 +0200

    Added More options

--- components/to-do-complete.tsx
+++ components/to-do-complete.tsx
@@ -1,23 +1,34 @@
-import type { NextPage } from "next";
-import { memo } from "react";
+// ToDoComplete.tsx
+import React from "react";
 
-type ToDoCompleteType = {
-  toDoImagine?: string;
+type ToDoCompleteProps = {
+  toDoImagine: string;
+  task: string;
+  isDone: boolean;
+  onClick: () => void;
 };
 
-const ToDoComplete: NextPage<ToDoCompleteType> = memo(({ toDoImagine }) => {
+const ToDoComplete: React.FC<ToDoCompleteProps> = ({
+  toDoImagine,
+  task,
+  isDone,
+  onClick,
+}) => {
   return (
-    <div className="self-stretch flex flex-row items-start justify-start gap-[0.63rem] text-left text-[1.13rem] text-slategray font-inter">
+    <div
+      className={`self-stretch flex flex-row items-start justify-start gap-[0.63rem] text-left text-[1.13rem] text-slategray font-inter ${
+        isDone ? "line-through text-gray-400" : ""
+      }`}
+      onClick={onClick}
+    >
       <img
-        className="relative w-[1.5rem] h-[1.5rem] object-cover"
+        className="relative w-[1.5rem] h-[1.5rem] object-cover cursor-pointer"
         alt=""
-        src={toDoImagine}
+        src={isDone ? "/image-1@2x.png" : toDoImagine}
       />
-      <div className="flex-1 relative leading-[1.5rem] font-medium">
-        Finish designs
-      </div>
+      <div className="flex-1 relative leading-[1.5rem] font-medium">{task}</div>
     </div>
   );
-});
+};
 
 export default ToDoComplete;
--- components/to-do-list-container.tsx
+++ components/to-do-list-container.tsx
@@ -1,21 +1,68 @@
-import type { NextPage } from "next";
+// ToDoListContainer.tsx
+import { NextPage } from "next";
+import { useState } from "react";
 import Header from "./header";
 import ToDoComplete from "./to-do-complete";
 
+type ToDoItem = {
+  id: number;
+  task: string;
+  isDone: boolean;
+};
+
 const ToDoListContainer: NextPage = () => {
+  const [toDoList, setToDoList] = useState<ToDoItem[]>([
+    { id: 1, task: "Create To Do List", isDone: false },
+    { id: 2, task: "Finish Your Job", isDone: false },
+  ]);
+
+  const addNewToDo = (task: string) => {
+    setToDoList([...toDoList, { id: Date.now(), task, isDone: false }]);
+  };
+
+  const toggleToDoStatus = (id: number) => {
+    setToDoList((prevList) =>
+      prevList.map((item) =>
+        item.id === id ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
+
+  // Split the list into completed and to be done items
+  const completedItems = toDoList.filter((item) => item.isDone);
+  const todoItems = toDoList.filter((item) => !item.isDone);
+
+  // Ensure "Finish Your Job" is at the bottom of the not-made-yet list
+  const sortedToDoList = [
+    ...todoItems.filter((item) => item.task !== "Finish Your Job"),
+    ...todoItems.filter((item) => item.task === "Finish Your Job"),
+    ...completedItems,
+  ];
+
   return (
     <div className="flex-1 bg-gray-300 flex flex-col py-[4rem] px-[3.56rem] items-start justify-start gap-[1.63rem] text-left text-[3.5rem] text-white font-inter sm:pl-[2rem] sm:pr-[2rem] sm:box-border">
       <Header />
       <div className="self-stretch relative bg-darkslategray h-[0.06rem]" />
-      <ToDoComplete toDoImagine="/image-1@2x.png" />
-      <ToDoComplete toDoImagine="/image-2@2x.png" />
+      {sortedToDoList.map((item) => (
+        <ToDoComplete
+          key={item.id}
+          toDoImagine={item.isDone ? "/image-1@2x.png" : "/image-2@2x.png"}
+          task={item.task}
+          isDone={item.isDone}
+          onClick={() => toggleToDoStatus(item.id)}
+        />
+      ))}
       <div className="self-stretch relative bg-darkslategray h-[0.06rem]" />
       <input
         className="[border:none] font-medium font-inter text-[1.13rem] bg-[transparent] self-stretch relative leading-[1.5rem] text-slategray text-left"
         type="text"
         placeholder="Add to do..."
-        maxLength
-        minLength
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.currentTarget.value) {
+            addNewToDo(e.currentTarget.value);
+            e.currentTarget.value = "";
+          }
+        }}
       />
     </div>
   );
